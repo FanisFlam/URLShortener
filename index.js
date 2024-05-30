d055da4a70c35544af0f1f347addf9427b58a213
Fanis Flam <fanisflamofficial@gmail.com>
2024-05-30 09:32:55 +0000

    Add body-parser middleware

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -18,6 +21,8 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.get('/api/shorturl')
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
